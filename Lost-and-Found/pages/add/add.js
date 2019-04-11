7c136cd56dd33209048e3e46aa131b0b4adf7b7a
HXBer <huangxber@gmail.com>
2019-04-12 01:16:50 +0800

    update

--- Lost-and-Found/pages/add/add.js
+++ Lost-and-Found/pages/add/add.js
@@ -12,40 +12,8 @@ Page({
     type_n:0,
     addr_array: ['位置', '教学楼', '精工园', '图书馆', '宿舍楼', '其他'],
     type_array: ['类型', '卡', '包', '书', '电子产品','伞', '其他'],
-    images: [],
     desc:'',
   },
-  onLoad(options) {
-    $init(this)
-  },
-
-  chooseImage(e) {
-    wx.chooseImage({
-      sizeType: ['original', 'compressed'],  //可选择原图或压缩后的图片
-      sourceType: ['album', 'camera'], //可选择性开放访问相册、相机
-      success: res => {
-        const images = this.data.images.concat(res.tempFilePaths)
-        // 限制最多只能留下3张照片
-        this.data.images = images.length <= 3 ? images : images.slice(0, 3)
-        $digest(this)
-      }
-    })
-  },
-  removeImage(e) {
-    const idx = e.target.dataset.idx
-    this.data.images.splice(idx, 1)
-    $digest(this)
-  },
-
-  handleImagePreview(e) {
-    const idx = e.target.dataset.idx
-    const images = this.data.images
-    wx.previewImage({
-      current: images[idx],  //当前预览的图片
-      urls: images,  //所有要预览的图片
-    })
-  },
-
 
   back:function()
   {
